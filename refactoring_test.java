db15ca911330ba5a5e0b1a85aa1e3c3f714d5c51
ngpoki120 <ngpoki120@yahoo.co.jp>
2017-01-11 11:10:07 +0900

    add file

---
+++ refactoring_test.java
@@ -0,0 +1,16 @@
+
+class refactoring_test{
+
+  public static void main(String args[]){
+
+  }
+
+  private static void conditional(){
+    int count = 0;
+    if(count == 0){
+      count = count + 2;
+    }else{
+      count = count + 1;
+    }
+  }
+}
