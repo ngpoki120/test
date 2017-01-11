fcdce793edda2240d204353459033172afee576f
ngpoki120 <ngpoki120@yahoo.co.jp>
2017-01-11 11:37:54 +0900

    fix

--- refactoring_test.java
+++ refactoring_test.java
@@ -2,15 +2,27 @@
 class refactoring_test{
 
   public static void main(String args[]){
-
+    conditional();
   }
 
   private static void conditional(){
     int count = 0;
-    if(count == 0){
-      count = count + 2;
+    if(isCountZero(count)){
+      count = thenCount(count);
     }else{
-      count = count + 1;
+      count = elseCount(count);
     }
+  }
+
+  private static boolean isCountZero(int count){
+    return count == 0;
+  }
+
+  private static int thenCount(int count){
+    return count + 2;
+  }
+
+  private static int elseCount(int count){
+    return count + 1;
   }
 }
